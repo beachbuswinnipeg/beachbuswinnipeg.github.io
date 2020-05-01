9fe839f84c843ef2c368c0a2205a3d72b188a5a3
ConorHoehn <64564162+ConorHoehn@users.noreply.github.com>
2020-05-01 08:06:27 -0500

    Added rudimentary points
    Added rudimentary points

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var app = new Vue({
     data: {
         mainHeadings: [
             {text: "There is no bus from Winnipeg to any beach."},
-            {text: "There should be a bus to the beach."},
+            {text: "There should be a bus to the beach.", points:[
+                {text: "Get cars off the road and thereby reduce greenhouse gas emissions."},
+                {text: "Get people into nature, with all the associated health benefits."}
+            ]},
             {text: "Why is there not a bus to the beach?"},
             {text: "How can we get a bus to the beach?"}
         ]
