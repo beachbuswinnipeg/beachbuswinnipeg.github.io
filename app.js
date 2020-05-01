2c7019534c6e9e6b3e6e8ac1a5e882ebd4b2cfbe
ConorHoehn <64564162+ConorHoehn@users.noreply.github.com>
2020-04-30 23:39:18 -0500

    Added headings
    Added headings

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Provincial funding.'
+        mainHeadings: [
+            {text: "There is no bus from Winnipeg to any beach."},
+            {text: "There should be a bus to the beach."},
+            {text: "Why is there not a bus to the beach?"},
+            {text: "How can we get a bus to the beach?"}
+        ]
     }        
 });
